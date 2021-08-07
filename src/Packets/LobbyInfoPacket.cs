45a1b9ca59d655089ad2e3375e1e1f6d6dc6f347
Tim Hanewich <tahanewich@live.com>
2021-08-07 19:41:34 -0400

    Updated Lobby info packet to 2021 spec

--- src/Packets/LobbyInfoPacket.cs
+++ src/Packets/LobbyInfoPacket.cs
@@ -6,7 +6,7 @@ namespace Codemasters.F1_2021
 {
     public class LobbyInfoPacket : Packet
     {
-        public byte NumPlayers { get; set; }
+        public byte NumberOfPlayers { get; set; }
         public LobbyInfoData[] LobbyPlayers { get; set; }
 
         public override void LoadBytes(byte[] bytes)
@@ -16,31 +16,17 @@ namespace Codemasters.F1_2021
             //Load header
             base.LoadBytes(BAM.NextBytes(24));
 
-            this.NumPlayers = BAM.NextByte();
-            this.LobbyPlayers = new LobbyInfoData[22];
+            //Get number of players
+            NumberOfPlayers = BAM.NextByte();
 
+            //Get lobby players
+            List<LobbyInfoData> ToAdd = new List<LobbyInfoData>();
             for (int i = 0; i < 22; i++)
             {
-                var lobbyInfoData = new LobbyInfoData
-                {
-                    AiControlled = BAM.NextByte() == 1,
-                    TeamId = CodemastersToolkit.GetTeamFromTeamId(BAM.NextByte()),
-                    Nationality = BAM.NextByte(),
-                    Name = ""
-                };
-                
-                for (var t = 1; t <= 48; t++)
-                {
-                    var currentChar = Convert.ToChar(BAM.NextByte());
-                    lobbyInfoData.Name += currentChar.ToString();
-                }
-
-                var readyStatusByte = BAM.NextByte();
-                lobbyInfoData.ReadyStatus = readyStatusByte == 0 ? ReadyStatus.NotReady :
-                    readyStatusByte == 1 ? ReadyStatus.Ready : ReadyStatus.Spectating;
-
-                this.LobbyPlayers[i] = lobbyInfoData;
+                ToAdd.Add(LobbyInfoData.Create(BAM.NextBytes(53)));
             }
+            LobbyPlayers = ToAdd.ToArray();
+
         }
         
         public class LobbyInfoData
@@ -49,12 +35,42 @@ namespace Codemasters.F1_2021
             public Team TeamId { get; set; }
             public byte Nationality { get; set; }
             public string Name { get; set; }
-
+            public byte CarNumber {get; set;}
             public ReadyStatus ReadyStatus { get; set; }
-            
+
+            public static LobbyInfoData Create(byte[] bytes)
+            {
+                LobbyInfoData ToReturn = new LobbyInfoData();
+
+                ByteArrayManager BAM = new ByteArrayManager(bytes);
+
+                //AI controlled
+                ToReturn.AiControlled = Convert.ToBoolean(BAM.NextByte());
+
+                //Team iD
+                ToReturn.TeamId = CodemastersToolkit.GetTeamFromTeamId(BAM.NextByte());
+
+                //Nationality
+                ToReturn.Nationality = BAM.NextByte();
+
+                //name
+                for (var t = 1; t <= 48; t++)
+                {
+                    var currentChar = Convert.ToChar(BAM.NextByte());
+                    ToReturn.Name += currentChar.ToString();
+                }
+
+                //Car number
+                ToReturn.CarNumber = BAM.NextByte();
+
+                //Ready status
+                ToReturn.ReadyStatus = (ReadyStatus)BAM.NextByte();
+
+                return ToReturn;
+            }
         }
 
-        public enum ReadyStatus : byte
+        public enum ReadyStatus
         {
             NotReady = 0,
             Ready = 1,
