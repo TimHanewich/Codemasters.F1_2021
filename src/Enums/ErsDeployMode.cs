4c00d17858c8918d5ded4534f733226e88d78e76
Tim Hanewich <tahanewich@live.com>
2021-08-07 14:04:02 -0400

    Added enums (imported from F1 2020)

---
+++ src/Enums/ErsDeployMode.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Codemasters.F1_2021
+{
+    public enum ErsDeployMode
+    {
+        None = 0,
+        Medium = 1,
+        Overtake = 2,
+        HotLap = 3
+    }
+}
---
+++ src/Enums/PacketType.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Codemasters.F1_2021
+{
+    public enum PacketType
+    {
+        Motion = 0, //1464 bytes
+        Session = 1, //251 bytes
+        Lap = 2, //1190 bytes
+        Event = 3, //35 bytes
+        Participants = 4, //1213 bytes
+        CarSetup = 5, //1102 bytes
+        CarTelemetry = 6, //1307 bytes
+        CarStatus = 7, //1344 bytes
+        FinalClassification = 8, //839 bytes    New to F1 2020.
+        LobbyInfo = 9 //1169 bytes   New to F1 2020
+    }
+}
