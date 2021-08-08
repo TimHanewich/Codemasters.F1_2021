eed5ef039605d622b8088c4cee9d69f966a1262e
Tim Hanewich <tahanewich@live.com>
2021-08-07 20:11:09 -0400

    Updated packet types enum with byte values

--- src/Enums/PacketType.cs
+++ src/Enums/PacketType.cs
@@ -5,16 +5,16 @@ namespace Codemasters.F1_2021
     public enum PacketType
     {
         Motion = 0, //1464 bytes
-        Session = 1, //251 bytes
-        Lap = 2, //1190 bytes
-        Event = 3, //35 bytes
-        Participants = 4, //1213 bytes
+        Session = 1, //625 bytes
+        Lap = 2, //970 bytes
+        Event = 3, //36 bytes
+        Participants = 4, //1257 bytes
         CarSetup = 5, //1102 bytes
-        CarTelemetry = 6, //1307 bytes
-        CarStatus = 7, //1344 bytes
+        CarTelemetry = 6, //1347 bytes
+        CarStatus = 7, //1058 bytes
         FinalClassification = 8, //839 bytes    New to F1 2020.
-        LobbyInfo = 9, //1169 bytes   New to F1 2020,
-        CarDamage = 10, //New to F1 2021
-        SessionHistory = 11 //New to F1 2021
+        LobbyInfo = 9, //1191 bytes   New to F1 2020,
+        CarDamage = 10, //882 bytes. New to F1 2021
+        SessionHistory = 11 //1155 bytes. New to F1 2021
     }
 }
